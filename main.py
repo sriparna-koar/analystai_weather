c6b65f2603b8ffecb86e64965e99263f881ac3c7
sriparna-koar <koarsk03@gmail.com>
2024-03-20 00:35:40 +0530

    first commit

--- main.py
+++ main.py
@@ -2,20 +2,52 @@ import datetime
 import requests
 import string
 from flask import Flask, render_template, request, redirect, url_for
-import os
-from dotenv import load_dotenv
-load_dotenv()
+
 
 OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/weather"
 OWM_FORECAST_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
 GEOCODING_API_ENDPOINT = "http://api.openweathermap.org/geo/1.0/direct"
-api_key = os.getenv("OWM_API_KEY")
-# api_key = os.environ.get("OWM_API_KEY")
+
+api_key = "27e15b0805a28f7ffa546b57b51f108c"
 
 app = Flask(__name__)
 
 
-# Display home page and get city name entered into search form
+def get_weather_data(city_name):
+    try:
+   
+        location_params = {"q": city_name, "appid": api_key}
+        location_response = requests.get(GEOCODING_API_ENDPOINT, params=location_params)
+        location_response.raise_for_status()
+        location_data = location_response.json()
+
+     
+        if location_data:
+            lat = location_data[0]['lat']
+            lon = location_data[0]['lon']
+        else:
+            raise ValueError("City coordinates not found")
+
+        weather_params = {"lat": lat, "lon": lon, "appid": api_key, "units": "metric"}
+        weather_response = requests.get(OWM_ENDPOINT, params=weather_params)
+        weather_response.raise_for_status()
+        weather_data = weather_response.json()
+
+
+        forecast_response = requests.get(OWM_FORECAST_ENDPOINT, params=weather_params)
+        forecast_response.raise_for_status()
+        forecast_data = forecast_response.json()
+
+        return weather_data, forecast_data
+
+    except requests.exceptions.RequestException as e:
+      
+        return None, None
+    except ValueError as ve:
+     
+        return None, None
+
+
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
@@ -24,74 +56,42 @@ def home():
     return render_template("index.html")
 
 
-# Display weather forecast for specific city using data from OpenWeather API
 @app.route("/<city>", methods=["GET", "POST"])
 def get_weather(city):
-    # Format city name and get current date to display on page
+
     city_name = string.capwords(city)
     today = datetime.datetime.now()
     current_date = today.strftime("%A, %B %d")
 
-    # Get latitude and longitude for city
-    location_params = {
-        "q": city_name,
-        "appid": api_key,
-        "limit": 3,
-    }
 
-    location_response = requests.get(GEOCODING_API_ENDPOINT, params=location_params)
-    location_data = location_response.json()
+    weather_data, forecast_data = get_weather_data(city_name)
 
-    # Prevent IndexError if user entered a city name with no coordinates by redirecting to error page
-    if not location_data:
-        return redirect(url_for("error"))
+    if weather_data and forecast_data:
+  
+        current_temp = round(weather_data['main']['temp'])
+        current_weather = weather_data['weather'][0]['main']
+        min_temp = round(weather_data['main']['temp_min'])
+        max_temp = round(weather_data['main']['temp_max'])
+        wind_speed = weather_data['wind']['speed']
+
+   
+        five_day_temp_list = [round(item['main']['temp']) for item in forecast_data['list'] if '12:00:00' in item['dt_txt']]
+        five_day_weather_list = [item['weather'][0]['main'] for item in forecast_data['list'] if '12:00:00' in item['dt_txt']]
+        five_day_unformatted = [today + datetime.timedelta(days=i) for i in range(5)]
+        five_day_dates_list = [date.strftime("%a") for date in five_day_unformatted]
+
+        return render_template("city.html", city_name=city_name, current_date=current_date, current_temp=current_temp,
+                               current_weather=current_weather, min_temp=min_temp, max_temp=max_temp, wind_speed=wind_speed,
+                               five_day_temp_list=five_day_temp_list, five_day_weather_list=five_day_weather_list,
+                               five_day_dates_list=five_day_dates_list)
     else:
-        lat = location_data[0]['lat']
-        lon = location_data[0]['lon']
-
-    # Get OpenWeather API data
-    weather_params = {
-        "lat": lat,
-        "lon": lon,
-        "appid": api_key,
-        "units": "metric",
-    }
-    weather_response = requests.get(OWM_ENDPOINT, weather_params)
-    weather_response.raise_for_status()
-    weather_data = weather_response.json()
-
-    # Get current weather data
-    current_temp = round(weather_data['main']['temp'])
-    current_weather = weather_data['weather'][0]['main']
-    min_temp = round(weather_data['main']['temp_min'])
-    max_temp = round(weather_data['main']['temp_max'])
-    wind_speed = weather_data['wind']['speed']
-
-    # Get five-day weather forecast data
-    forecast_response = requests.get(OWM_FORECAST_ENDPOINT, weather_params)
-    forecast_data = forecast_response.json()
-
-    # Make lists of temperature and weather description data to show user
-    five_day_temp_list = [round(item['main']['temp']) for item in forecast_data['list'] if '12:00:00' in item['dt_txt']]
-    five_day_weather_list = [item['weather'][0]['main'] for item in forecast_data['list']
-                             if '12:00:00' in item['dt_txt']]
-
-    # Get next four weekdays to show user alongside weather data
-    five_day_unformatted = [today, today + datetime.timedelta(days=1), today + datetime.timedelta(days=2),
-                            today + datetime.timedelta(days=3), today + datetime.timedelta(days=4)]
-    five_day_dates_list = [date.strftime("%a") for date in five_day_unformatted]
-
-    return render_template("city.html", city_name=city_name, current_date=current_date, current_temp=current_temp,
-                           current_weather=current_weather, min_temp=min_temp, max_temp=max_temp, wind_speed=wind_speed,
-                           five_day_temp_list=five_day_temp_list, five_day_weather_list=five_day_weather_list,
-                           five_day_dates_list=five_day_dates_list)
+  
+        return redirect(url_for("error"))
 
 
-# Display error page for invalid input
 @app.route("/error")
 def error():
     return render_template("error.html")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
